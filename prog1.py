e574cdf7d3c3e577c403336c4999c78dfeaaaec0
akashbindal <akashbindal15@gmail.com>
2014-11-12 16:08:26 +0530

    prog1.py updated
    This will train upon 70% of the total data and will predict output for remaining 30%

--- prog1.py
+++ prog1.py
@@ -10,17 +10,20 @@ from prog2 import *
 data  = loadtxt('ex2data1.txt', delimiter=',')
 
 numrows = len(data)
+numrow_train = int(numrows*0.7)
 numcols = len(data[0])
 
 #separate the data
-X = data[:,0:numcols-1]
-Y = data[:,numcols-1]
+X_train = data[0:numrow_train,0:numcols-1]
+Y_train = data[0:numrow_train,numcols-1]
 
+X_pre = data[numrow_train:numrows,0:numcols-1]
+Y_pre = data[numrow_train:numrows,numcols-1]
 #add the bias term to data
-m = numrows
+m = int(numrows*0.7)
 n = numcols-1
 X0 = ones((m,1))
-X = hstack((X0,X))
+X_train = hstack((X0,X_train))
 
 #initialise the fitting parametrers
 initial_theta = zeros( (n + 1, 1) );
@@ -29,13 +32,13 @@ initial_theta = zeros( (n + 1, 1) );
 Lambda = .3;
 
 #compute initial cost and gradient
-cost = costFunction(initial_theta, X, Y, Lambda)
+cost = costFunction(initial_theta, X_train, Y_train, Lambda)
 
 options = {'full_output': True, 'maxiter': 400}
 
 
-theta, cost, _, _, _ =  optimize.fmin(lambda t: costFunction(t, X, Y, Lambda ), initial_theta, **options)
+theta, cost, _, _, _ =  optimize.fmin(lambda t: costFunction(t, X_train, Y_train, Lambda ), initial_theta, **options)
 
-p = predict(theta, X)
+p = predict(theta, X_pre)
 
-print('Train Accuracy:', (p == Y).mean() * 100)
+print('Train Accuracy:', (p == Y_pre).mean() * 100)
