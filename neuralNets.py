63072969421d0813db94daca0c7a3144725aa886
poweltalwar <poweltalwar@gmail.com>
2015-01-16 15:55:45 +0530

    neural nets, view image and predict

--- neuralNets.py
+++ neuralNets.py
@@ -3,11 +3,31 @@ import sys
 from numpy import *
 from numpy import random
 from scipy import io
+import pylab
+from matplotlib import *
+import matplotlib.pyplot as plt
 from pybrain.structure import *
 from pybrain.tools.shortcuts import buildNetwork
-from pybrain.datasets import ClassificationDataSet
+from pybrain.datasets import ClassificationDataSet, SupervisedDataSet
 from pybrain.utilities import percentError
 from pybrain.supervised.trainers import BackpropTrainer
+
+
+def plotData(X, Y):
+    '''plots the input data '''
+
+    # plot any one case (20x20 image) from the input
+    # the image matrix will have to be transposed to be viewed correcty
+    # cmap shows the color map
+    c = random.randint(0, X.shape[0])    
+    image = array(X[c,:])
+    plt.imshow((image.reshape(20,20)).T, cmap = 'Greys')
+    plt.show()
+    
+    #plot the same ouptut case
+    print("true number is \t" + str(Y[c]))
+    
+    return c
 
 
 def splitData(alldata, factor):
@@ -80,12 +100,12 @@ net.sortModules()
 #activate by giving input
 #n.activate(X[0])
 
-allData = ClassificationDataSet(n, 10, nb_classes=numLabels)
-#Y2 = convertToOneOfMany(Y)
+allData = SupervisedDataSet(n, 10)
+Y2 = convertToOneOfMany(Y)
 
 for i in range(m):
     inData = X[i,:]
-    outData = Y[i, :]
+    outData = Y2[i, :]
     allData.addSample(inData, outData)
 
 #allData.setField('input', X)
@@ -94,19 +114,27 @@ for i in range(m):
 #separate training and testing data
 dataTrain, dataTest = allData.splitWithProportion(0.70)
 
-dataTrain._convertToOneOfMany()
-dataTest._convertToOneOfMany()
+#dataTrain._convertToOneOfMany()
+#dataTest._convertToOneOfMany()
 
 #train = BackpropTrainer(net, learningrate=0.01, momentum=0.9, verbose=True)
 train = BackpropTrainer(net, dataset=dataTrain)
-train.trainUntilConvergence(dataset=dataTrain)
-#train.trainEpochs(10)
-
+#train.trainUntilConvergence(dataset=dataTrain)
+'''train.trainEpochs(50)
+resTrain = percentError( train.testOnClassData ( dataset=dataTest ),True )
+resTest = percentError( train.testOnClassData ( dataset=dataTrain ),True )
 '''
-for i in range(20):
+
+for i in range(50):
     train.trainEpochs(1)
-    trnresult = percentError(train.testOnClassData(), dataTrain)
-    tstresult = percentError(train.testOnClassData(dataset=dataTest), dataTest)
+    trnresult = percentError(train.testOnClassData(dataset=dataTest), True)
+    tstresult = percentError(train.testOnClassData(dataset=dataTrain), True)
     print("epoch: %4d" % train.totalepochs,"  train error: %5.2f%%" % trnresult,
               "  test error: %5.2f%%" % tstresult)
-'''
+
+testNum = plotData(X, Y)
+X1 = X[testNum, :]
+prediction = net.activate(X1)
+print(prediction)
+p = argmax(prediction, axis=0)
+print("predicted output is \t" + str(p))
--- prog1.py
+++ prog1.py
@@ -1,14 +1,36 @@
 import os
 import sys
+import numpy
 from numpy import *
 from numpy import random
 from scipy import optimize as op
 from scipy import io
-from pylab import *
+import pylab
+from matplotlib import *
+import matplotlib.pyplot as plt
 from tempfile import TemporaryFile
 
 
+def plotData(X, Y):
+    '''plots the input data '''
+
+    # plot any one case (20x20 image) from the input
+    # the image matrix will have to be transposed to be viewed correcty
+    # cmap shows the color map
+    c = random.randint(0, X.shape[0])    
+    image = array(X[c,:])
+    plt.imshow((image.reshape(20,20)).T, cmap = 'Greys')
+    plt.show()
+    
+    #plot the same ouptut case
+    print("true number is \t" + str(Y[c]))
+    
+    return c
+
+
 def standardize(X):
+    '''standardize data if need be. This function brings all the attributes
+    to zero mean and unit variance'''
     mn = mean(X, axis=0)
     stdev = std(X, axis=0)
     [m, n] = shape(X)
@@ -46,27 +68,35 @@ def costFunction(theta, X, y, Lambda):
 
     J = -1 * transpose(y).dot(log(h)) - transpose(1 - y).dot(log(1 - h))
     J = J / m
-    theta = np.array(theta)
+    theta = array(theta)
     J = J + (Lambda/(2*m)) * sum(theta * theta)
 
     return J
 
 
 def oneVsAll(X, y, numLabels, Lambda):
+    '''initialise the theta parameters and train them on the training set for
+    all labels'''
     [m, n] = shape(X)
     allTheta = zeros((numLabels, n + 1))
-    [rows, cols] = shape(allTheta)
-    X = hstack((ones((m, 1)), X))
-    initTheta = randn(n + 1, 1)
 
-    theta = op.fmin_cg(lambda t: costFunction(t, X, (y == 10).astype(int), Lambda), initTheta,  maxiter=100, gtol=0.001, epsilon=.0001)
-    theta = np.array(theta)
+    #append the bias term to input data
+    X = hstack((ones((m, 1)), X))
+    #initial value of theta, further to be optimized
+    initTheta = random.randn(n + 1, 1)
+
+    theta = op.fmin_cg(lambda t: costFunction(t, X, (y == 10).astype(int), Lambda), initTheta,
+                         maxiter=100, gtol=0.001, epsilon=.0001)
+    theta = array(theta)
+    
+    #append in main array of theta
     allTheta[0, :] = theta
 
+    #optimize using fmin_cg
     for i in range(1, numLabels):
         print(i)
         theta = op.fmin_cg(lambda t: costFunction(t, X, (y == (i)).astype(int), Lambda), initTheta,  maxiter=100, gtol=0.001, epsilon=.0001)
-        theta = np.array(theta)
+        theta = array(theta)
         allTheta[i, :] = theta
 
     return allTheta
@@ -84,36 +114,58 @@ def predictOneVsAll(allTheta, X):
     return (p)
 
 
+#load data here
 data = io.loadmat('ex3data1.mat')
 X = data['X']
 [m, n] = shape(X)
-X = X[950:1050,:]
 Y = data['y']
-Y = Y.flatten()
 Y = reshape(Y, (len(Y), -1))
-Y = Y[950:1050,:]
+Y[Y == 10] = 0 
 
+#shuffle data
 x1 = hstack((X, Y))
-shuffle(x1)
-X = x1[:, 0:n-1]
+random.shuffle(x1)
+X = x1[:, 0:-1]
 Y = x1[:, -1]
 
+X = X[1:100,:]
+Y = Y[1:100]
+
+#divide into training and test set, division of 70:30
 numrows = len(X)
 numrow_train = int(numrows * 0.7)
 
+#training set
 X_train = X[0:numrow_train, :]
 Y_train = Y[0:numrow_train]
 Y_train = Y_train.flatten()
 
+#test set
 X_test = X[numrow_train:numrows, :]
 Y_test = Y[numrow_train:numrows]
 Y_test = Y_test.flatten()
 
-numLabels = 10
+#get theta parameters for multiclass logistic regression
+numLabels = len(unique(Y))
 Lambda = .3
 allTheta = oneVsAll(X, Y, numLabels, Lambda)
 
+#after training, run on test set and check accuracy
 pred = predictOneVsAll(allTheta, X_test)
 correct = (pred == (Y_test % 10)).astype(double)
 acc = mean(correct) * 100
 print(acc)
+
+testNum = plotData(X, Y)
+X1 = X[testNum, :]
+X1 = reshape(X1, (len(X1), -1))
+X1 = X1.transpose()
+[m, n] = shape(array(X1))
+X1 = hstack((ones((m, 1)), X1))
+h = sigmoid(X1.dot(allTheta.transpose()))
+h = h.transpose()
+print(h)
+p = argmax(h, axis=0)
+print(" predicted output is \t" + str(p[0]))
+
+
